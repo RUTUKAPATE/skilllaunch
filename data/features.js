cbc06e49b03579461ed831d4e6ad85c718455d0a
Rutuja Kapate <rutuja16kapate@gmail.com>
2025-03-26 22:50:16 +0530

    chore: Added missing features data file

---
+++ data/features.js
@@ -0,0 +1,14 @@
+import { BrainCircuit, Briefcase, LineChart, ScrollText } from "lucide-react";
+
+export const features = [
+  {
+    title: "COLLEGE",
+    description:
+      "Our features will help you in all those things that te college doesn't help you in. From study plans to topic explanations, all will be taken care of.",
+  },
+  {
+    title: "CAREER",
+    description:
+      "Create a resume from scratch can be time consuming. We will help you by creating an AI generated resume according to your skills.",
+  },
+];
